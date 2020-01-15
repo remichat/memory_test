1bd0488a42b3a68c071a8b63b8f9797dfffb1342
Rémi Chatenay <remi.chatenay@gadz.org>
2020-01-15 17:51:12 +0100

    added root route to revenue_country

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'country_revenues#show'
   get '/country_revenues/:country', to: 'country_revenues#show'
 end

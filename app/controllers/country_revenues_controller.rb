bd67d26f701857b9a7a3a1cdc5bc2b8af150f2a4
Rémi Chatenay <remi.chatenay@gadz.org>
2020-01-15 17:48:59 +0100

    fixed all countries path

--- app/controllers/country_revenues_controller.rb
+++ app/controllers/country_revenues_controller.rb
@@ -7,14 +7,12 @@ class CountryRevenuesController < ApplicationController
       country_revenues = Revenue.where('LOWER(country) = ? AND quantity > 0', @selected_country)
     else
       country_revenues = Revenue.where('quantity > 0')
-      @selected_country = all
+      @selected_country = "all"
     end
 
-    # @total_revenue = country_revenues.reduce(0) {|memo, revenue| memo + revenue.revenue }.round
     @total_revenue = country_revenues.sum(:revenue).round
     @avg_revenue_per_order = @total_revenue.fdiv(country_revenues.distinct.count(:order_id)).round(2)
     @nb_customers = country_revenues.distinct.count(:customer_id)
     @revenue_per_month = country_revenues.group_by_month(:date).sum(:revenue)
-
   end
 end
